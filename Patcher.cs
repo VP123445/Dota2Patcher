309088d0042f2d507d4eac5ecc9e10c6e08e7ada
Александр Вольф <fake.wolf@2ch.hk>
2021-04-15 00:56:31 +1000

    Добавьте файлы проекта.

---
+++ Patcher.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace Dota2Patcher
+{
+    static class Patcher
+    {
+        private static readonly byte[][][] sigEngine = {
+            new byte[2][] {
+                new byte[] { 0x74, 0x54, 0x48, 0x8B, 0x0D, 0xCC, 0xCC, 0xCC, 0xCC, 0x48, 0x8B, 0x01, 0xFF, 0x90, 0xCC, 0xCC, 0xCC, 0xCC, 0x84, 0xC0, 0x74, 0x40, 0x48, 0x8B, 0x0D },
+                new byte[] { 0xEB }
+            }
+        };
+        
+        private static int FindBytes(byte[] src, byte[] find)
+        {
+            int index = -1;
+            int matchIndex = 0;
+            
+            for (int i = 0; i < src.Length; i++)
+            {
+                if (find[matchIndex] == 0xCC || src[i] == find[matchIndex])
+                {
+                    if (matchIndex == (find.Length - 1))
+                    {
+                        index = i - matchIndex;
+                        break;
+                    }
+                    matchIndex++;
+                }
+                else if (src[i] == find[0])
+                {
+                    matchIndex = 1;
+                }
+                else
+                {
+                    matchIndex = 0;
+                }
+            }
+            return index;
+        }
+        
+        private static byte[] ReplaceBytes(byte[] src, byte[] search, byte[] repl)
+        {
+            byte[] dst = null;
+            int index = FindBytes(src, search);
+            if (index >= 0)
+            {
+                dst = new byte[src.Length];
+                Buffer.BlockCopy(src, 0, dst, 0, index);
+                Buffer.BlockCopy(repl, 0, dst, index, repl.Length);
+                Buffer.BlockCopy(
+                    src,
+                    index + repl.Length,
+                    dst,
+                    index + repl.Length,
+                    src.Length - (index + repl.Length));
+            }
+            return dst;
+        }
+        
+        public static void Patch()
+        {
+            string pathToEngine = Program.path_to_engine2.ToString();
+            byte[] engineBytes = File.ReadAllBytes(pathToEngine);
+            
+            foreach (byte[][] find in sigEngine)
+            {
+                if (engineBytes != null)
+                    engineBytes = ReplaceBytes(engineBytes, /* original */ find[0], /* cracked */ find[1]);
+            }
+            if (engineBytes != null)
+                File.WriteAllBytes(pathToEngine, engineBytes);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+
+namespace Dota2Patcher
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Patching...");
+            Determine_dota_path();
+            Patcher.Patch();
+            Console.WriteLine("Patched!");
+            Console.ReadLine();
+        }
+
+        internal static DirectoryInfo path_to_engine2;
+
+        internal static void Determine_dota_path()
+        {
+            using (RegistryKey registryKey1 = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64))
+            {
+                using (RegistryKey registryKey2 = registryKey1.OpenSubKey("Software\\Classes\\dota2\\Shell\\Open\\Command"))
+                {
+                    object obj = registryKey2.GetValue("");
+                    if (!(obj.GetType().Name == "String"))
+                        return;
+                    path_to_engine2 = new DirectoryInfo(obj.ToString().Split(new string[1] {
+            "\""
+          }, StringSplitOptions.None)[1].Split(new string[1] {
+            "game\\bin\\win"
+          }, StringSplitOptions.None)[0] + "game\\bin\\win64\\engine2.dll");
+                }
+            }
+        }
+
+    }
+}
